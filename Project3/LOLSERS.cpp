75628fac22fd698cb4c437965a4f6de5fb56207f
NubSM <qingyuanking123@hotmail.com>
2015-05-01 15:18:02 -0400

    Commit 2
    TEST

---
+++ Project3/LOLSERS.cpp
@@ -0,0 +1,13 @@
+#include "LOLSERS.h"
+
+
+LOLSERS::LOLSERS()
+{
+	LOL = 50;
+	LOLL = false;
+}
+
+
+LOLSERS::~LOLSERS()
+{
+}
---
+++ Project3/LOLSERS.h
@@ -0,0 +1,10 @@
+#pragma once
+class LOLSERS
+{
+public:
+	int LOL;
+	bool LOLL;
+	LOLSERS();
+	~LOLSERS();
+};
+
